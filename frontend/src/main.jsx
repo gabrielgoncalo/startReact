fafae75291e05ed1043798d81a72d8859d8f6430
gabriel goncalo <gabriel.goncalo.705@ufrn.edu.br>
2025-07-03 17:30:12 -0300

    criando o header

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react' // ajuda a detectar problemas de renderização, avisos...
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css' //importando o css principal. h1, p, a... 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
