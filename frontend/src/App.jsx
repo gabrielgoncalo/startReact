83b0c41230d56758c568e5e0e457ffe8ee6ab7ea
gabriel goncalo <gabriel.goncalo.705@ufrn.edu.br>
2025-07-05 18:37:24 -0300

    add campo de postagem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 
 function App() {
   return (
-    <header class= "main-header">
+    <div>
+    <header className="main-header">
       <h1>MyPlace</h1>
-      
     </header>
+      <form className="campoDeMsg" action="/enviar" method="post">
+        <input type="text" id="mensagem" name="mensagem" placeholder="Digite sua mensagem" /><br />
+        <button type="submit">Enviar</button>
+      </form>
+      </div>
   )
-
 }
 
 export default App
