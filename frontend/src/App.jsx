c679390fc2f5c33d006bdb6410032ba59e24bb53
gabriel goncalo <gabriel.goncalo.705@ufrn.edu.br>
2025-07-08 16:51:02 -0300

    telinha ta ficando show

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
 import { useState } from 'react';
+import { FiSettings } from 'react-icons/fi'; // Ícone de engrenagem
 
 function App() {
-  const [mensagem, setMensagem] = useState('');
+  const [mensagem, setMensagem] = useState(''); //contador 
+  const [aberto, setAberto] = useState(false); //botao configuração
+
+  const toggleConfig = () => {
+    setAberto(!aberto);
+  }
+
 
   const handleSubmit = (e) => {
     e.preventDefault();//impede o envio padrao
@@ -12,17 +19,42 @@ function App() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="form-container">
+    <>
+    <header className="main-header">
+      <div className='logo-header'>
+        <h1>MyPlace</h1>
+      </div> 
+
+     <button onClick={toggleConfig} className="btn-config">
+      <FiSettings className="icone" />
+     </button>
+
+     {aberto && (
+      <>
+        <div className="overlay" onClick={() => setAberto(false)}></div>
+        <div className="menu-config">
+          <p>Perfil</p>
+          <p>login</p>
+          <p>Logout</p>
+        </div>
+      </>
+     )}
+     </header>
+
+
+     <form onSubmit={handleSubmit} className="campoDeMsg">
+    
       <textarea
         maxLength={280} 
         value={mensagem}
         onChange={(e)=> setMensagem(e.target.value)}
         placeholder="Digite sua mensagem..."
-        className="campoDeMsg"
+        className="textarea"
       />
-      <div className="contador">{mensagem.length}</div>
+      <div className="contador">{mensagem.length}/280</div>
       <button type="submit">Enviar</button>
-     </form> 
+     </form>
+     </>
   );
 }
 
