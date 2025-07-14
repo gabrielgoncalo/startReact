47c3d7d2563d4b14c44733062d2c0b1ebd0ee1e9
gabriel goncalo <gabriel.goncalo.705@ufrn.edu.br>
2025-07-14 17:30:20 -0300

    criando rotas para cadastro e login de usuario

--- backend/src/routes/msgRoute.js
+++ backend/src/routes/msgRoute.js
@@ -2,7 +2,12 @@ const express= require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 
+
+router.post("/register", postController.criarUser);
+router.post("/login", postController.loginUser);
 router.post("/", postController.criarPost);
 router.get("/", postController.listarPost);
+
+
 
 module.exports = router;
